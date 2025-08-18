7f54b530a8ce14e5b96474615598be7ceffbe0f8
ZBEP <ZBEP@LIST.RU>
2025-08-18 03:24:15 +0300

    up

--- assets/socket_edge_tts.js
+++ assets/socket_edge_tts.js
@@ -148,14 +148,21 @@
 	start_works() {
 		//console.log("Start works...")//console.log(this.my_filename + " " + this.my_filenum + " start works...")
 		if ("WebSocket" in window) {
+			const SEC_MS_GEC_VERSION = "1-130.0.2849.68";
+			const secMsGec = this.generateSecMsGec();
+			
 			this.socket = new WebSocket(
 				"wss://speech.platform.bing.com/consumer/speech/synthesize/" +
 				"readaloud/edge/v1?TrustedClientToken=" +
 				"6A5AA1D4EAFF4E9FB37E23D68491D6F4" +
-				"&ConnectionId=" + this.connect_id())
-			this.socket.addEventListener('open', this.onSocketOpen.bind(this))
-			this.socket.addEventListener('message', this.onSocketMessage.bind(this))
-			this.socket.addEventListener('close', this.onSocketClose.bind(this))
+				"&Sec-MS-GEC=" + secMsGec +
+				"&Sec-MS-GEC-Version=" + SEC_MS_GEC_VERSION +
+				"&ConnectionId=" + this.connect_id()
+			);
+			
+			this.socket.addEventListener('open', this.onSocketOpen.bind(this));
+			this.socket.addEventListener('message', this.onSocketMessage.bind(this));
+			this.socket.addEventListener('close', this.onSocketClose.bind(this));
 		} else {
 			console.log("WebSocket NOT supported by your Browser!");
 		}
@@ -178,6 +185,104 @@
 		});
 		return uuid.replace(/-/g, '');
 	}
+	
+	
+	sha256(ascii) {
+		function rightRotate(value, amount) {
+			return (value >>> amount) | (value << (32 - amount));
+		}
+		
+		var mathPow = Math.pow;
+		var maxWord = mathPow(2, 32);
+		var lengthProperty = 'length';
+		var i, j;
+		var result = '';
+
+		var words = [];
+		var asciiBitLength = ascii[lengthProperty] * 8;
+		
+		var hash = this.sha256.h = this.sha256.h || [];
+		var k = this.sha256.k = this.sha256.k || [];
+		var primeCounter = k[lengthProperty];
+
+		var isComposite = {};
+		for (var candidate = 2; primeCounter < 64; candidate++) {
+			if (!isComposite[candidate]) {
+				for (i = 0; i < 313; i += candidate) {
+					isComposite[i] = candidate;
+				}
+				hash[primeCounter] = (mathPow(candidate, .5) * maxWord) | 0;
+				k[primeCounter++] = (mathPow(candidate, 1 / 3) * maxWord) | 0;
+			}
+		}
+		
+		ascii += '\x80';
+		while (ascii[lengthProperty] % 64 - 56) ascii += '\x00';
+		for (i = 0; i < ascii[lengthProperty]; i++) {
+			j = ascii.charCodeAt(i);
+			if (j >> 8) return;
+			words[i >> 2] |= j << ((3 - i) % 4) * 8;
+		}
+		words[words[lengthProperty]] = ((asciiBitLength / maxWord) | 0);
+		words[words[lengthProperty]] = (asciiBitLength);
+		
+		for (j = 0; j < words[lengthProperty];) {
+			var w = words.slice(j, j += 16);
+			var oldHash = hash;
+			hash = hash.slice(0, 8);
+			
+			for (i = 0; i < 64; i++) {
+				var w15 = w[i - 15], w2 = w[i - 2];
+
+				var a = hash[0], e = hash[4];
+				var temp1 = hash[7]
+					+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25))
+					+ ((e & hash[5]) ^ ((~e) & hash[6]))
+					+ k[i]
+					+ (w[i] = (i < 16) ? w[i] : (
+							w[i - 16]
+							+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15 >>> 3))
+							+ w[i - 7]
+							+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2 >>> 10))
+						) | 0
+					);
+				var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22))
+					+ ((a & hash[1]) ^ (a & hash[2]) ^ (hash[1] & hash[2]));
+				
+				hash = [(temp1 + temp2) | 0].concat(hash);
+				hash[4] = (hash[4] + temp1) | 0;
+			}
+			
+			for (i = 0; i < 8; i++) {
+				hash[i] = (hash[i] + oldHash[i]) | 0;
+			}
+		}
+		
+		for (i = 0; i < 8; i++) {
+			for (j = 3; j + 1; j--) {
+				var b = (hash[i] >> (j * 8)) & 255;
+				result += ((b < 16) ? 0 : '') + b.toString(16);
+			}
+		}
+		return result;
+	}
+
+	generateSecMsGec() {
+		const WIN_EPOCH = 11644473600;
+		const S_TO_NS = 1e9;
+		const TRUSTED_CLIENT_TOKEN = "6A5AA1D4EAFF4E9FB37E23D68491D6F4";
+		
+		let ticks = Date.now() / 1000;
+		ticks += WIN_EPOCH;
+		ticks -= ticks % 300;
+		ticks *= S_TO_NS / 100;
+		
+		const strToHash = Math.floor(ticks) + TRUSTED_CLIENT_TOKEN;
+		
+		return this.sha256(strToHash).toUpperCase();
+	}
+	
+	
 	
 	async saveFiles(blob) {
 		if (this.start_save == false) {
